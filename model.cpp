ddcee3caf5f01b2043b0df2de9cd5c748cc9bb94
Wouter Heerwegh <wouter.heerwegh@student.kuleuven.be>
2018-11-21 13:33:53 +0100

    Priority queue added

--- model.cpp
+++ model.cpp
@@ -9,18 +9,21 @@ Model::~Model(){
 
 }
 
- vector<shared_ptr<tile_t>> Model::makeMap(vector<unique_ptr<Tile>> & tiles){
 
-     vector<shared_ptr<tile_t>> map;
+
+vector<shared_ptr<tile_t>> Model::makeMap(vector<unique_ptr<Tile>> & tiles){
+
+    vector<shared_ptr<tile_t>> map;
 
     for(unique_ptr<Tile> & tile : tiles){
         shared_ptr<tile_t> t = shared_ptr<tile_t>(new tile_t);
-        t->t = make_shared<Tile>(*tile);
+        t->t = tile.get();
         t->f = 0;
         t->g = 0;
         t->h = 0;
         t->open = false;
         t->closed = false;
+        t->prev = nullptr;
         map.push_back(t);
     }
 
@@ -38,16 +41,55 @@ int Model::aStar(shared_ptr<tile_t> start, shared_ptr<tile_t> goal, vector<share
         return WALL;
     }
     start->closed = true;
-    make_heap(open.begin(), open.end());
+    //make_heap(open.begin(), open.end(), Model::comp);
     shared_ptr<tile_t> current = start;
+    goal->f = 5;
+
+    open.push(start);
+    open.push(goal);
 
     cout << "start value " << val << " at " << x << ", " << y << endl;
     cout << "goal value " << goal->t->getValue() << " at " << goal->t->getXPos() << ", " << goal->t->getYPos() << endl;
-    /*while(current != goal){
+
+    printqueue(open);
+    //while(!open.empty()){
         //run algorithm
 
 
-    }*/
+    //}
 
     return SUCCESS;
+}
+
+
+void Model::checkNeighbours(shared_ptr<tile_t> t){
+
+}
+
+double Model::heuristic(shared_ptr<tile_t> a, shared_ptr<tile_t> b){
+    //octile distance
+    double dx = abs(a->t->getXPos() - b->t->getXPos());
+    double dy = abs(a->t->getYPos() - b->t->getYPos());
+    double dist;
+    double min;
+    double max;
+    if(dx < dy){
+        min = dx;
+        max = dy;
+        dist = ((dy-dx) + (SQRT * dx));
+    } else {
+        dist = ((dx-dy) + (SQRT * dy));
+        min = dy;
+        max = dx;
+    }
+
+    return dist;
+}
+
+void Model::printqueue(priority_queue<shared_ptr<tile_t>, vector<shared_ptr<tile_t>>, CustomCompare> q){
+    while(!q.empty()){
+        int val = q.top()->t->getXPos();
+        cout << val << endl;
+        q.pop();
+    }
 }
--- model.h
+++ model.h
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <limits>
 #include <iostream>
+#include <queue>
 #include "world.h"
 #include "world_global.h"
 
@@ -13,11 +14,16 @@ using namespace std;
 #define SUCCESS 0
 #define WALL 1
 #define NO_PATH 2
+#define SQRT  1.4142135
 
 
-typedef struct {
-    shared_ptr<Tile> t;
-    Tile * prev;
+struct tile;
+
+typedef tile tile_t;
+
+typedef struct tile{
+    Tile * t;
+    tile_t * prev;
     int f;
     int g;
     int h;
@@ -25,16 +31,28 @@ typedef struct {
     bool closed;
 } tile_t;
 
+struct CustomCompare
+{
+    bool operator()(shared_ptr<tile_t> a, shared_ptr<tile_t> b)
+    {
+        return a->f > b->f;
+    }
+};
 
 class Model
 {
 public:
     Model();
     ~Model();
-     vector<shared_ptr<tile_t>> makeMap(vector<unique_ptr<Tile>> & tiles);
+    vector<shared_ptr<tile_t>> makeMap(vector<unique_ptr<Tile>> & tiles);
+
     int aStar(shared_ptr<tile_t> start, shared_ptr<tile_t> goal,  vector<shared_ptr<tile_t>> & map);
 private:
-    vector<shared_ptr<tile_t>> open;
+    priority_queue<shared_ptr<tile_t>, vector<shared_ptr<tile_t>>, CustomCompare> open;
+    void printqueue(priority_queue<shared_ptr<tile_t>, vector<shared_ptr<tile_t>>, CustomCompare> q);
+    double heuristic(shared_ptr<tile_t> a, shared_ptr<tile_t> b);
+    void checkNeighbours(shared_ptr<tile_t> t);
+
 };
 
 
